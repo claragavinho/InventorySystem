2130ec0aca468f1a6f06d522ba5977e71b37e29c
Clara Gavinho <clara.gavinho@gmail.com>
2024-07-25 16:26:22 -0600

    Player Controller + Camera

---
+++ A2_ClaraGavinho/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float _moveSpeed = 5f; //player speed
+    [SerializeField]
+    private float _gravity = -20f; //how strong is gravity when we jump
+    [SerializeField]
+    private float _rotationSpeed = 90f; //how quickly the player will turn
+    [SerializeField]
+    private float _jumpSpeed = 15f; //how fast the player will jump
+
+    private float horizontalInput;
+    private float verticalInput;
+
+    Vector3 moveVelocity; //forward velocity
+    Vector3 turnVelocity; //rotate velocity
+    Vector3 direction; //the direction we will go
+
+    private CharacterController _controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        direction = new Vector3(horizontalInput, 0, verticalInput); // WASD for direction
+        direction.Normalize();//set vector magnitude to 1.0
+
+        if (_controller.isGrounded) //must be on a surface to jump
+        {
+            moveVelocity = transform.forward * _moveSpeed * verticalInput;
+            turnVelocity = transform.up * _rotationSpeed * horizontalInput;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                moveVelocity.y = _jumpSpeed;
+            }
+        }
+
+        moveVelocity.y += _gravity * Time.deltaTime; //gravity
+
+        _controller.Move(moveVelocity * Time.deltaTime); //move the player regulated by constant time
+        transform.Rotate(turnVelocity * Time.deltaTime); //turn the player regulated by constant time
+    }
+}
